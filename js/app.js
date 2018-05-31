38f62fcb9eaa3d173c2ea91a69ef5aa506b0f15a
Gibbor Munyama <gibbormunyama@gmail.com>
2018-05-31 18:06:36 +0100

    fix: Creating an array holding list of cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
- const listOfCards = document.querySelectorAll('li.card');
+const listOfCards = ["fa fa-diamond","fa fa-diamond","fa fa-paper-plane-o","fa fa-paper-plane-o","fa fa-anchor","fa fa-anchor","fa fa-bolt","fa fa-bolt","fa fa-cube","fa fa-cube","fa fa-leaf","fa fa-leaf","fa fa-bicycle","fa fa-bicycle","fa fa-bomb","fa fa-bomb" ]
 
 /*
  * Display the cards on the page

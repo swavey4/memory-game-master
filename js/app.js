de5806f30c857ea62b03baccc74975b4152ebb8a
Gibbor Munyama <gibbormunyama@gmail.com>
2018-06-07 16:40:58 +0100

    feat: Functionality that resets the game

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
-const listOfCards = [ "fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb" ]
+let listOfCards = [ "fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb" ]
 
 /*
  * Display the cards on the page
@@ -28,15 +28,17 @@ function shuffle(array) {
 const container = document.querySelector('.deck');
 let listOpen = [];
 
-listOfCards.forEach(function(i){
-  const listElement = document.createElement('li');
-  listElement.classList.add('card');
-  listElement.innerHTML = `<i class= "${i}" > </i>`
-  container.appendChild(listElement);
+function createList(){
+  listOfCards.forEach(function(i){
+    const listElement = document.createElement('li');
+      listElement.classList.add('card');
+      listElement.innerHTML = `<i class= "${i}" > </i>`
+      container.appendChild(listElement);
 
   clickElement(listElement);
+  reset();
 });
-
+}
 
 function clickElement(listElement){
   listElement.addEventListener('click', function(){
@@ -61,6 +63,17 @@ function clickElement(listElement){
      }
     })
 }
+
+function reset(){
+  const reset = document.querySelector('.restart');
+  reset.addEventListener('click', function(){
+    container.innerHTML = '';
+    createList();
+    listOpen = [];
+  })
+}
+
+createList();
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
